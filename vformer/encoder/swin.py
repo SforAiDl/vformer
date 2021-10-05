78039c8dc6221bbb4913e706887cb947d76c1b5a
Abhijit_asus <f20190041@goa.bits-pilani.ac.in>
2021-10-05 21:11:54 +0530

    swinT encoder

---
+++ vformer/encoder/swin.py
@@ -0,0 +1,168 @@
+import torch.nn as nn
+from torch.utils.checkpoint import checkpoint as checkpoint
+
+from ..attention.swin import WindowAttention
+from ..utils.utils import (
+    DropPath,
+    create_mask,
+    cyclicshift,
+    window_partition,
+    window_reverse,
+)
+from .nn import FeedForward
+
+
+class SwinEncoderBlock(nn.Module):
+    """"""
+
+    def __init__(
+        self,
+        dim,
+        input_resolution,
+        num_heads,
+        window_size,
+        shift_size=0,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qk_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        act_layer=nn.GELU,
+        norm_layer=nn.LayerNorm,
+    ):
+        super(SwinEncoderBlock, self).__init__()
+        self.dim = dim
+        self.input_resolution = input_resolution
+        self.num_heads = num_heads
+        self.window_size = window_size
+        self.mlp_ratio = mlp_ratio
+        self.shift_size = shift_size
+        if min(self.input_resolution) <= self.window_size:
+            self.shift_size = 0
+            self.window_size = min(self.input_resolution)
+        assert (
+            0 <= self.shift_size < self.window_size
+        ), " `shift_size` must be in 0 to `window_size` "
+        self.norm1 = norm_layer(dim)
+        self.attn = WindowAttention(
+            dim=dim,
+            window_size=(window_size, window_size),
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            qk_scale=qk_scale,
+            attn_drop=attn_drop,
+            proj_drop=drop,
+        )
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        hidden_dim = int(dim * mlp_ratio)
+        self.mlp = FeedForward(
+            dim=dim, hidden_dim=hidden_dim, act_layer=act_layer, p_dropout=drop
+        )
+
+        if self.shift_size > 0:
+            attn_mask = create_mask(
+                self.window_size,
+                self.shift_size,
+                H=self.input_resolution[0],
+                W=self.input_resolution[1],
+            )
+        else:
+            attn_mask = None
+        self.register_buffer("attn_mask", attn_mask)
+
+    def forward(self, x):
+        H, W = self.input_resolution
+        B, L, C = x.shape
+        assert L == H * W, "input tensor shape is not correct; "
+
+        skip_connection = x
+
+        x = self.norm1(x)
+        x = x.view(B, H, W, C)
+
+        if self.shift_size > 0:
+            shifted_x = cyclicshift(x, shift_size=-self.shift_size)
+        else:
+            shifted_x = x
+
+        x_windows = window_partition(shifted_x, self.window_size).view(
+            -1, self.window_size * self.window_size, C
+        )
+
+        attn_windows = self.attn(x_windows, mask=self.attn_mask).view(
+            -1, self.shift_size, self.window_size, C
+        )
+
+        shifted_x = window_reverse(attn_windows, self.window_size, H, W)
+
+        if self.shift_size > 0:
+            x = cyclicshift(shifted_x, shift_size=self.shift_size).view(B, H * W, C)
+        else:
+            x = shifted_x.view(B, H * W, C)
+
+        x = skip_connection + self.drop_path(x)
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+
+        return x
+
+
+class SwinEncoder(nn.Module):
+    """"""
+
+    def __init__(
+        self,
+        dim,
+        input_resolution,
+        depth,
+        num_heads,
+        window_size,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        qkv_scale=None,
+        drop=0.0,
+        attn_drop=0.0,
+        drop_path=0.0,
+        norm_layer=nn.LayerNorm,
+        downsample=None,
+        use_checkpoint=False,
+    ):
+        super(SwinEncoder, self).__init__()
+        self.depth = depth
+        self.use_checkpoint = use_checkpoint
+
+        self.blocks = nn.ModuleList(
+            [
+                SwinEncoderBlock(
+                    dim=dim,
+                    input_resolution=input_resolution,
+                    num_heads=num_heads,
+                    window_size=window_size,
+                    shift_size=0 if (i % 2 == 0) else window_size // 2,
+                    mlp_ratio=mlp_ratio,
+                    qkv_bias=qkv_bias,
+                    qk_scale=qkv_scale,
+                    drop=drop,
+                    attn_drop=attn_drop,
+                    drop_path=drop_path[i]
+                    if isinstance(drop_path, list)
+                    else drop_path,
+                    norm_layer=norm_layer,
+                )
+                for i in range(depth)
+            ]
+        )
+        if downsample is not None:
+            self.downsample = downsample(
+                input_resolution, dim=dim, norm_layer=norm_layer
+            )
+        else:
+            self.downsample = None
+
+    def forward(self, x):
+        for blk in self.blocks:
+            x = blk(x)
+            if self.downsample is not None:
+                x = self.downsample(x)
