a693306f5fb579d99751da8e09a766b6d9803e1c
aditya agrawal <f20201381@goa.bits-pilani.ac.in>
2021-10-08 22:53:23 +0530

    hopefully-final

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -1,16 +1,25 @@
 import torch
+
 import vformer.viz as viz
 from vformer.models import VanillaViT
-img = torch.randn(1,3, 224, 224)
-layer = "attend"
-model = VanillaViT(img_size=256, patch_size=32, n_classes=10, in_channels=3)
-model_attention_grad_rollout = viz.VITAttentionGradRollout(model,layer)
-model_attention_rollout_mean = viz.VITAttentionRollout(model,layer)
-model_attention_rollout_max = viz.VITAttentionRollout(model,layer,"max")
-model_attention_rollout_min = viz.VITAttentionRollout(model,layer,"min")
-_ = model(img)
-_ = model_attention_rollout_mean(img)
-_ = model_attention_rollout_max(img)
-_ = model_attention_rollout_min(img)
-_ = model_attention_grad_rollout(img,0)
 
+
+def test_attention_rollout():
+    img = torch.randn(1, 3, 224, 224)
+    layer = "attend"
+    model = VanillaViT(img_size=256, patch_size=32, n_classes=10, in_channels=3)
+    model_attention_rollout_mean = viz.VITAttentionRollout(model, layer)
+    model_attention_rollout_max = viz.VITAttentionRollout(model, layer, "max")
+    model_attention_rollout_min = viz.VITAttentionRollout(model, layer, "min")
+    _ = model(img)
+    _ = model_attention_rollout_mean(img)
+    _ = model_attention_rollout_max(img)
+    _ = model_attention_rollout_min(img)
+
+
+def test_attention_grad_rollout():
+    img = torch.randn(1, 3, 224, 224)
+    layer = "attend"
+    model = VanillaViT(img_size=256, patch_size=32, n_classes=10, in_channels=3)
+    model_attention_grad_rollout = viz.VITAttentionGradRollout(model, layer)
+    _ = model_attention_grad_rollout(img, 0)
--- vformer/viz/vit_rollout.py
+++ vformer/viz/vit_rollout.py
@@ -1,10 +1,13 @@
+import sys
+
+import numpy
+import numpy as np
 import torch
 from PIL import Image
-import numpy
-import sys
 from torchvision import transforms
-import numpy as np
-#import cv2
+
+# import cv2
+
 
 def rollout(attentions, discard_ratio, head_fusion):
     result = torch.eye(attentions[0].size(-1))
@@ -22,28 +25,34 @@ def rollout(attentions, discard_ratio, head_fusion):
             # Drop the lowest attentions, but
             # don't drop the class token
             flat = attention_heads_fused.view(attention_heads_fused.size(0), -1)
-            _, indices = flat.topk(int(flat.size(-1)*discard_ratio), -1, False)
+            _, indices = flat.topk(int(flat.size(-1) * discard_ratio), -1, False)
             indices = indices[indices != 0]
             flat[0, indices] = 0
 
             I = torch.eye(attention_heads_fused.size(-1))
-            a = (attention_heads_fused + 1.0*I)/2
+            a = (attention_heads_fused + 1.0 * I) / 2
             a = a / a.sum(dim=-1)
 
             result = torch.matmul(a, result)
-    
+
     # Look at the total attention between the class token,
     # and the image patches
-    mask = result[0, 0 , 1 :]
+    mask = result[0, 0, 1:]
     # In case of 224x224 image, this brings us from 196 to 14
-    width = int(mask.size(-1)**0.5)
+    width = int(mask.size(-1) ** 0.5)
     mask = mask.reshape(width, width).numpy()
     mask = mask / np.max(mask)
-    return mask    
+    return mask
+
 
 class VITAttentionRollout:
-    def __init__(self, model, attention_layer_name='attn_drop', head_fusion="mean",
-        discard_ratio=0.9):
+    def __init__(
+        self,
+        model,
+        attention_layer_name="attn_drop",
+        head_fusion="mean",
+        discard_ratio=0.9,
+    ):
         self.model = model
         self.head_fusion = head_fusion
         self.discard_ratio = discard_ratio
