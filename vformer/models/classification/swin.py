aaab5d5989ebfce13557eca13e1f31ef1e276f5f
Abhijit_asus <f20190041@goa.bits-pilani.ac.in>
2021-10-09 08:36:32 +0530

    added arxiv link in the description of classification/swin.py

--- vformer/models/classification/swin.py
+++ vformer/models/classification/swin.py
@@ -13,6 +13,7 @@ class SwinTransformer(BaseClassificationModel):
     Implementation of `Swin Transformer: Hierarchical Vision Transformer using Shifted Windows`
     https://arxiv.org/abs/2103.14030v1
 
+
     Parameters:
     -----------
     img_size: int
