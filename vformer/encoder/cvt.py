bfab0d39c9edfaab3759b884bfed3210bf869585
Abhijit_asus <f20190041@goa.bits-pilani.ac.in>
2021-11-27 16:38:55 +0530

    [WIP] *added embedding, (in paper they refer this as tokenizer) *added encoder and classification model *added tests

---
+++ vformer/encoder/cvt.py
@@ -0,0 +1,63 @@
+import torch
+import torch.functional as F
+import torch.nn as nn
+from timm.models.layers import DropPath
+
+from ..attention import VanillaSelfAttention
+from ..decoder import MLPDecoder
+from ..functional.norm import PreNorm
+from .nn import FeedForward
+
+
+class CVTEncoderBlock(nn.Module):
+    def __init__(
+        self,
+        dim,
+        num_head,
+        p_dropout,
+        attn_dropout,
+        hidden_dim=None,
+        out_dim=None,
+        drop_path_rate=0.0,
+        **kwargs,
+    ):
+        super(CVTEncoderBlock, self).__init__()
+        self.encoder = nn.ModuleList([])
+        self.encoder.append(
+            nn.ModuleList(
+                [
+                    PreNorm(
+                        dim=dim,
+                        fn=VanillaSelfAttention(
+                            dim=dim,
+                            heads=num_head,
+                            p_dropout=attn_dropout,
+                        ),
+                    ),
+                    PreNorm(
+                        dim=dim,
+                        fn=FeedForward(
+                            dim=dim,
+                            hidden_dim=hidden_dim,
+                            out_dim=out_dim,
+                            p_dropout=p_dropout,
+                            **kwargs,
+                        ),
+                    ),
+                ]
+            )
+        )
+        self.norm = nn.LayerNorm(dim)
+        self.drop_path = (
+            DropPath(drop_prob=drop_path_rate)
+            if drop_path_rate > 0.0
+            else nn.Identity()
+        )
+
+    def forward(self, x):
+        for attn, ff in self.encoder:
+            x = attn(x) + x
+            x = self.norm(x)
+            x = ff(x) + x
+        x = x + self.drop_path(x)
+        return x
--- vformer/models/classification/__init__.py
+++ vformer/models/classification/__init__.py
@@ -2,3 +2,4 @@ from .cross import CrossViT
 from .pyramid import PVTClassification, PVTClassificationV2
 from .swin import SwinTransformer
 from .vanilla import VanillaViT
+from .cvt import CVT
