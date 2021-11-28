23a81f89e306b118e2bc9e0cf81d8660086c59ad
Rishav Mukherji <72412977+Amapocho@users.noreply.github.com>
2021-11-28 16:39:42 +0530

    Update imports

--- vformer/attention/multiscale.py
+++ vformer/attention/multiscale.py
@@ -1,10 +1,6 @@
 import numpy
 import torch
 import torch.nn as nn
-from timm.models.layers import DropPath
-
-from vformer.vformer.encoder.nn import FeedForward as Mlp
-
 
 def attention_pool(tensor, pool, thw_shape, has_cls_embed=True, norm=None):
     """
