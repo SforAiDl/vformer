03fc0f11d3174b77ec77c442a18f387ffd4c2e62
Rishav Mukherji <72412977+Amapocho@users.noreply.github.com>
2021-11-28 11:25:43 +0530

    Create droppath.py

---
+++ vformer/common/droppath.py
@@ -0,0 +1,9 @@
+class DropPath(nn.Module):
+    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)."""
+
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
