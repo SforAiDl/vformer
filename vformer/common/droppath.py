fb41ba360c5450ea4dc8503a6f5b5bcf65c25f43
Rishav Mukherji <72412977+Amapocho@users.noreply.github.com>
2021-11-28 11:37:48 +0530

    Update docstring

--- vformer/common/droppath.py
+++ vformer/common/droppath.py
@@ -1,5 +1,11 @@
 class DropPath(nn.Module):
-    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)."""
+    """
+    Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks)
+    Parameters:
+    -----------
+    dim: float, optional
+         Probability of dropping paths
+    """
 
     def __init__(self, drop_prob=None):
         super(DropPath, self).__init__()
