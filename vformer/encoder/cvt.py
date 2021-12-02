d7d5f7c36c21e708c5d5ad408fb2ef68a0c2ea6c
Abhijit_asus <f20190041@goa.bits-pilani.ac.in>
2021-12-02 14:29:16 +0530

    Minor edit

--- vformer/encoder/cvt.py
+++ vformer/encoder/cvt.py
@@ -67,10 +67,10 @@ class CVTEncoderBlock(nn.Module):
             else nn.Identity()
         )
 
+
     def forward(self, x):
         for attn, ff in self.encoder:
-            x = attn(x) + x
-            x = self.norm(x)
-            x = ff(x) + x
-        x = x + self.drop_path(x)
+            x = self.drop_path(attn(x)) + x
+            x = self.drop_path(ff(self.norm(x))) + x
+
         return x
