b88417c4127faec78f84bb31ba7a46a064e6bd93
Abhijit Deo <72816663+abhi-glitchhg@users.noreply.github.com>
2021-12-31 08:35:19 +0530

    conflict resolve

--- vformer/models/classification/swin.py
+++ vformer/models/classification/swin.py
@@ -138,9 +138,7 @@ class SwinTransformer(BaseClassificationModel):
         self.pool = nn.AdaptiveAvgPool1d(1)
         self.norm = norm_layer(num_features) if norm_layer is not None else nn.Identity
 
-
-
-
+        self.pos_drop = nn.Dropout(p=p_dropout)
 
     def forward(self, x):
         """
