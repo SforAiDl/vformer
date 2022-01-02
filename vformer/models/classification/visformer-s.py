b0f7b230e60b97eabb392a23f8b96a29b572f7f9
aditya agrawal <f20201381@goa.bits-pilani.ac.in>
2022-01-02 22:46:32 +0530

    visformer-implementation

--- vformer/models/classification/visformer-s.py
+++ vformer/models/classification/visformer-s.py
@@ -45,11 +45,12 @@ class Attention_Block(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(in_dim, in_dim * 4, kernel_size=1, bias=False)
         self.conv2 = nn.Conv2d(in_dim * 4, out_dim, kernel_size=1, bias=False)
-        self.attn = VanillaSelfAttention()
+        self.attn = VanillaSelfAttention(in_dim)
         self.norm1 = nn.BatchNorm2d(in_dim)
         self.norm2 = nn.BatchNorm2d(in_dim)
 
     def forward(self, x):
+        x = einops.rearrange(x, "b c h w -> b c (h w)")
         xt = x
         x = self.norm1(x)
         x = self.attn(x)
@@ -64,12 +65,20 @@ class Attention_Block(nn.Module):
 
 
 class Visformer(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        initial_channels,
-    ):
+    def __init__(self, in_channels, initial_channels, depth, config):
         super().__init__()
-        self.stem = nn.Conv2d(
-            in_channels, initial_channels, kernel_size=7, padding=3, stride=2
+        self.stem = nn.ModuleList(
+            [
+                nn.Conv2d(
+                    in_channels, initial_channels, kernel_size=7, padding=3, stride=2
+                )
+            ]
         )
+        self.st1 = nn.ModuleList([Conv_Block(192, 192) for i in range(depth[0])])
+        self.st2 = nn.ModuleList([Attention_Block(384, 384) for i in range(depth[1])])
+        self.st3 = nn.ModuleList([Attention_Block(768, 768) for i in range(depth[2])])
+        for i in range(depth):
+            if config[i] == 0:
+                self.stem.extend([[Conv_Block(192, 192) for j in range(depth[i])]])
+            else:
+                self.stem.extend([Attention_Block(384, 384) for j in range(depth[i])])
