f9b58da2accce2c28cabeba984457adbb3cc1b43
Rishav Mukherji <72412977+Amapocho@users.noreply.github.com>
2022-01-04 14:23:16 +0530

    Create multiscale.py

---
+++ vformer/models/classification/multiscale.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+from vformer.common import BaseClassificationModel
+from vformer.decoder.mlp import MLPDecoder
+from vformer.encoder.multiscale import  MultiScaleBlock
+
+attention_block = MultiScaleBlock(
+                dim=embed_dim,
+                dim_out=dim_out,
+                num_heads=num_heads,
+                mlp_ratio=mlp_ratio,
+                qkv_bias=qkv_bias,
+                drop_rate=self.drop_rate,
+                drop_path=dpr[i],
+                norm_layer=norm_layer,
+                kernel_q=pool_q[i] if len(pool_q) > i else [],
+                kernel_kv=pool_kv[i] if len(pool_kv) > i else [],
+                stride_q=stride_q[i] if len(stride_q) > i else [],
+                stride_kv=stride_kv[i] if len(stride_kv) > i else [],
+                mode=mode,
+                has_cls_embed=self.cls_embed_on,
+                pool_first=pool_first,
+            )
