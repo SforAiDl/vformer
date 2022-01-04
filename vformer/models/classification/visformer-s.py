6a17f0d2bdb61b3d535f13b828f4cad2b55da5f5
aditya agrawal <f20201381@goa.bits-pilani.ac.in>
2022-01-04 19:45:46 +0530

    framework-complete-few-changes-required

--- vformer/models/classification/visformer-s.py
+++ vformer/models/classification/visformer-s.py
@@ -1,14 +1,17 @@
 import einops
 import torch
 import torch.nn as nn
+from torchsummary import summary
 
 from vformer.attention import VanillaSelfAttention
 
 
-# need to add dropout
+# need to add dropout,scale,head
+# editted number of heads, head dim_s   need to change
 class Conv_Block(nn.Module):
-    def __init__(self, in_channels, group=1, activation=nn.ReLU, drop=0.0):
+    def __init__(self, in_channels, group=8, activation=nn.GELU, drop=0.0):
         super(Conv_Block, self).__init__()
+        self.norm1 = nn.BatchNorm2d(in_channels)
         self.conv1 = nn.Conv2d(in_channels, in_channels * 2, kernel_size=1, bias=False)
         self.act1 = activation()
         self.conv2 = nn.Conv2d(
@@ -25,6 +28,7 @@ class Conv_Block(nn.Module):
 
     def forward(self, x):
         xt = x
+        xt = self.norm1(xt)
         x = self.conv1(x)
         x = self.act1(x)
         x = self.drop(x)
@@ -41,7 +45,7 @@ class Attention_Block(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(channels, channels * 4, kernel_size=1, bias=False)
         self.conv2 = nn.Conv2d(channels * 4, channels, kernel_size=1, bias=False)
-        self.attn = VanillaSelfAttention(channels)
+        self.attn = VanillaSelfAttention(channels, num_heads=6, head_dim=channels // 6)
         self.norm1 = nn.BatchNorm2d(channels)
         self.norm2 = nn.BatchNorm2d(channels)
 
@@ -63,9 +67,12 @@ class Attention_Block(nn.Module):
 
 
 class Visformer(nn.Module):
-    def __init__(self, image_size, depth: list, config: str, channel_config: list):
+    def __init__(
+        self, image_size, n_classes, depth: list, config: str, channel_config: list
+    ):
         super().__init__()
         q = 0
+        self.linear = nn.Linear(channel_config[-1], n_classes)
         if isinstance(image_size, int):
             image_size = (image_size, image_size)
         image_size = list(image_size)
@@ -77,21 +84,24 @@ class Visformer(nn.Module):
                     kernel_size=7,
                     padding=3,
                     stride=2,
-                )
+                    bias=False,
+                ),
+                nn.BatchNorm2d(channel_config[q + 1]),
+                nn.ReLU(inplace=True),
             ]
         )
         q += 1
         image_size = [i // 2 for i in image_size]
         for i in range(len(depth)):
-            if config[i] == 0:
+            if config[i] == "0":
                 self.stem.extend(
                     [Conv_Block(channel_config[q]) for j in range(depth[i])]
                 )
-            elif config[i] == 1:
+            elif config[i] == "1":
                 self.stem.extend(
                     [Attention_Block(channel_config[q]) for j in range(depth[i])]
                 )
-            elif config[i] == 2:
+            elif config[i] == "2":
                 self.stem.extend(
                     [
                         nn.Conv2d(
@@ -99,14 +109,24 @@ class Visformer(nn.Module):
                             channel_config[q + 1],
                             kernel_size=depth[i],
                             stride=depth[i],
-                        )
+                        ),
+                        nn.BatchNorm2d(channel_config[q + 1]),
                     ]
                 )
                 q += 1
-                image_size = image_size // depth[i]
+                image_size = [k // depth[i] for k in image_size]
             assert q < len(channel_config), "channel configuration not complete"
+        self.stem.extend([nn.BatchNorm2d(channel_config[-1]), nn.AdaptiveAvgPool2d(1)])
 
     def forward(self, x):
         for i in self.stem:
             x = i(x)
+        x.squeeze_(2).squeeze_(2)
+        x = self.linear(x)
         return x
+
+
+Visformer_s = Visformer(244, 1000, [4, 7, 2, 4, 2, 4], "202121", [3, 32, 192, 384, 768])
+Visformer_s_s2 = Visformer(
+    244, 1000, [2, 1, 2, 10, 2, 14, 2, 3], "20202121", [3, 32, 64, 128, 256, 512]
+)
