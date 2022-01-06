409c1dd4d29b3c7a4c50088bb22b7741a07b1fbd
Abhijit Deo <72816663+abhi-glitchhg@users.noreply.github.com>
2022-01-06 19:12:55 +0530

    [WIP] Refactoring DPT

--- vformer/models/dense/dpt.py
+++ vformer/models/dense/dpt.py
@@ -1,162 +1,272 @@
 import torch
 import torch.nn as nn
+import types
 
-from ...utils.dpt_utils import (
-    FeatureFusionBlock_custom,
-    Interpolate,
-    _make_encoder,
-    forward_vit,
-)
+from ...utils.dpt_utils import get_readout_oper, Transpose
+
 from ...utils.registry import MODEL_REGISTRY
 
 
-@MODEL_REGISTRY.register()
-class DPT(nn.Module):
-    def __init__(self):
-        super(DPT, self).__init__()
 
-    def __init__(
-        self,
-        head,
-        features=256,
-        backbone="vitb_rn50_384",
-        readout="project",
-        channels_last=False,
-        use_bn=False,
-        enable_attention_hooks=False,
-    ):
-        super(DPT, self).__init__()
+activations = {}
 
-        self.channels_last = channels_last
 
-        hooks = {
-            "vitb_rn50_384": [0, 1, 8, 11],
-            "vitb16_384": [2, 5, 8, 11],
-            "vitl16_384": [5, 11, 17, 23],
-            "vitb16_384_vf": [2, 5, 8, 11],
-            "vitl16_384_vf": [5, 11, 17, 23],
-        }
+def get_activation(name):
+    def hook(model, input, output):
+        activations[name] = output
 
-        # Instantiate backbone and reassemble blocks
-        self.pretrained, self.scratch = _make_encoder(
-            backbone,
-            features,
-            False,  # Set to true of you want to train from scratch, uses ImageNet weights
-            groups=1,
-            expand=False,
-            exportable=False,
-            hooks=hooks[backbone],
-            use_readout=readout,
-            enable_attention_hooks=enable_attention_hooks,
+    return hook
+
+
+attention = {}
+
+
+def get_attention(name):
+    def hook(module, input, output):
+        x = input[0]
+        B, N, C = x.shape
+        qkv = (
+            module.qkv(x)
+            .reshape(B, N, 3, module.num_heads, C // module.num_heads)
+            .permute(2, 0, 3, 1, 4)
         )
+        q, k, v = (
+            qkv[0],
+            qkv[1],
+            qkv[2],
+        )  # make torchscript happy (cannot use tensor as tuple)
 
-        self.scratch.refinenet1 = FeatureFusionBlock_custom(
-            features,
-            nn.ReLU(False),
-            deconv=False,
-            bn=use_bn,
-            expand=False,
-            align_corners=True,
-        )
+        attn = (q @ k.transpose(-2, -1)) * module.scale
 
-        self.scratch.refinenet2 = FeatureFusionBlock_custom(
-            features,
-            nn.ReLU(False),
-            deconv=False,
-            bn=use_bn,
-            expand=False,
-            align_corners=True,
-        )
+        attn = attn.softmax(dim=-1)  # [:,:,1,1:]
+        attention[name] = attn
 
-        self.scratch.refinenet3 = FeatureFusionBlock_custom(
-            features,
-            nn.ReLU(False),
-            deconv=False,
-            bn=use_bn,
-            expand=False,
-            align_corners=True,
-        )
+    return hook
 
-        self.scratch.refinenet4 = FeatureFusionBlock_custom(
-            features,
-            nn.ReLU(False),
-            deconv=False,
-            bn=use_bn,
-            expand=False,
-            align_corners=True,
-        )
+##############
 
-        self.scratch.output_conv = head
+#annother dpt
 
-    def forward(self, x):
-        if self.channels_last == True:
-            x.contiguous(memory_format=torch.channels_last)
-
-        layer_1, layer_2, layer_3, layer_4 = forward_vit(self.pretrained, x)
-        layer_1_rn = self.scratch.layer1_rn(layer_1)
-        layer_2_rn = self.scratch.layer2_rn(layer_2)
-        layer_3_rn = self.scratch.layer3_rn(layer_3)
-        layer_4_rn = self.scratch.layer4_rn(layer_4)
-
-        path_4 = self.scratch.refinenet4(layer_4_rn)
-        path_3 = self.scratch.refinenet3(path_4, layer_3_rn)
-        path_2 = self.scratch.refinenet2(path_3, layer_2_rn)
-        path_1 = self.scratch.refinenet1(path_2, layer_1_rn)
-
-        out = self.scratch.output_conv(path_1)
-
-        return out
-
-
-@MODEL_REGISTRY.register()
-class DPTDepthModel(DPT):
-    def __init__(
-        self,
-        backbone,
-        features=256,
-        readout="project",
+class DPTdept(nn.Module):
+    def __init__(self,backbone,features,readout,
         channels_last=False,
         use_bn=False,
         enable_attention_hooks=False,
         non_negative=True,
         scale=1.0,
         shift=0.0,
-        invert=False,
-        **kwargs
-    ):
-        features = kwargs["features"] if "features" in kwargs else 256
+        invert=False,):
+        super(DPTdept, self).__init__()
+        features_dict = {} # these will contain the parameters for vitl16 and vitb16
+        self.model = MODEL_REGISTRY.get(backbone)(**features_dict)
+        self._register_hooks_and_add_postprocess()
+        self._make_scratch()
 
-        self.scale = scale
-        self.shift = shift
-        self.invert = invert
+    def _register_hooks_and_add_postprocess(self,
+                        size=(384, 384),
+                        features=(96, 192, 384, 768),
+                        hooks=(2, 5, 8, 11),
+                        use_readout="ignore",
+                        enable_attention_hooks=False,
+                        vit_features=768,
+                        start_index=1
+                        ):
 
-        head = nn.Sequential(
-            nn.Conv2d(features, features // 2, kernel_size=3, stride=1, padding=1),
-            Interpolate(scale_factor=2, mode="bilinear", align_corners=True),
-            nn.Conv2d(features // 2, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(32, 1, kernel_size=1, stride=1, padding=0),
-            nn.ReLU(True) if non_negative else nn.Identity(),
-            nn.Identity(),
+        for i in range(4):
+            self.model.encoder.encoder[hooks[i]][0].fn.register_forward_hook(get_activation(str(i+1)))
+
+        self.activations = activations
+
+        if enable_attention_hooks:
+            for i in range(4):
+                self.model.encoder.encoder[hooks[i]][0].fn.register_forward_hook(get_attention(f"attn_{str(i+1)}"))
+
+        readout_oper = get_readout_oper(vit_features, features, use_readout, start_index)
+
+        # 32, 48, 136, 384
+
+        self.act_postprocess1 = nn.Sequential(
+            readout_oper[0],
+            Transpose(1, 2),
+            nn.Unflatten(2, torch.Size([size[0] // 16, size[1] // 16])),
+            nn.Conv2d(
+                in_channels=vit_features,
+                out_channels=features[0],
+                kernel_size=1,
+                stride=1,
+                padding=0,
+            ),
+            nn.ConvTranspose2d(
+                in_channels=features[0],
+                out_channels=features[0],
+                kernel_size=4,
+                stride=4,
+                padding=0,
+                bias=True,
+                dilation=1,
+                groups=1,
+            ),
         )
 
-        super().__init__(
-            head=head,
-            backbone=backbone,
-            features=features,
-            readout=readout,
-            channels_last=channels_last,
-            use_bn=use_bn,
-            enable_attention_hooks=enable_attention_hooks,
+        self.act_postprocess2 = nn.Sequential(
+            readout_oper[1],
+            Transpose(1, 2),
+            nn.Unflatten(2, torch.Size([size[0] // 16, size[1] // 16])),
+            nn.Conv2d(
+                in_channels=vit_features,
+                out_channels=features[1],
+                kernel_size=1,
+                stride=1,
+                padding=0,
+            ),
+            nn.ConvTranspose2d(
+                in_channels=features[1],
+                out_channels=features[1],
+                kernel_size=2,
+                stride=2,
+                padding=0,
+                bias=True,
+                dilation=1,
+                groups=1,
+            ),
         )
 
-    def forward(self, x):
-        inv_depth = super().forward(x).squeeze(dim=1)
+        self.act_postprocess3 = nn.Sequential(
+            readout_oper[2],
+            Transpose(1, 2),
+            nn.Unflatten(2, torch.Size([size[0] // 16, size[1] // 16])),
+            nn.Conv2d(
+                in_channels=vit_features,
+                out_channels=features[2],
+                kernel_size=1,
+                stride=1,
+                padding=0,
+            ),
+        )
 
-        if self.invert:
-            depth = self.scale * inv_depth + self.shift
-            depth[depth < 1e-8] = 1e-8
-            depth = 1.0 / depth
-            return depth
+        self.act_postprocess4 = nn.Sequential(
+            readout_oper[3],
+            Transpose(1, 2),
+            nn.Unflatten(2, torch.Size([size[0] // 16, size[1] // 16])),
+            nn.Conv2d(
+                in_channels=vit_features,
+                out_channels=features[3],
+                kernel_size=1,
+                stride=1,
+                padding=0,
+            ),
+            nn.Conv2d(
+                in_channels=features[3],
+                out_channels=features[3],
+                kernel_size=3,
+                stride=2,
+                padding=1,
+            ),
+        )
+
+        self.model.start_index = start_index
+        self.model.patch_size = [16, 16]
+
+        # We inject this function into the VisionTransformer instances so that
+        # we can use it with interpolated position embeddings without modifying the library source.
+        self.model.forward_flex = types.MethodType(self.forward_flex, self.model)
+        self.model._resize_pos_embed = types.MethodType(
+            self._resize_pos_embed, self.model
+        )
+
+    def _make_scratch(self,in_shape, out_shape:int , groups=1, expand=False):
+        self.scratch = nn.Module()
+        out_shape = [out_shape]*4
+
+        for i in range(4):
+            layer = nn.Conv2d(
+                in_shape[i],
+                out_shape[i] *(2)**(i) if expand else out_shape[i],
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                bias=False,
+                groups=groups
+            )
+            setattr(self.scratch,f"layer{i+1}_rn",layer)
+
+
+    def forward_vit(self, x):
+        """
+
+        Parameters
+        -----------
+        """
+
+        b, c, h, w = x.shape
+
+        glob = self.model.forward_flex(x)
+
+        layer_1 = self.activations["1"]
+        layer_2 = self.activations["2"]
+        layer_3 = self.activations["3"]
+        layer_4 = self.activations["4"]
+
+        layer_1 = self.act_postprocess1[0:2](layer_1)
+        layer_2 = self.act_postprocess2[0:2](layer_2)
+        layer_3 = self.act_postprocess3[0:2](layer_3)
+        layer_4 = self.act_postprocess4[0:2](layer_4)
+
+        unflatten = nn.Sequential(
+            nn.Unflatten(
+                2,
+                torch.Size(
+                    [
+                        h // self.model.patch_size[1],
+                        w // self.model.patch_size[0],
+                    ]
+                ),
+            )
+        )
+
+        if layer_1.ndim == 3:
+            layer_1 = unflatten(layer_1)
+        if layer_2.ndim == 3:
+            layer_2 = unflatten(layer_2)
+        if layer_3.ndim == 3:
+            layer_3 = unflatten(layer_3)
+        if layer_4.ndim == 3:
+            layer_4 = unflatten(layer_4)
+
+        layer_1 = self.act_postprocess1[3: len(self.act_postprocess1)](layer_1)
+        layer_2 = self.act_postprocess2[3: len(self.act_postprocess2)](layer_2)
+        layer_3 = self.act_postprocess3[3: len(self.act_postprocess3)](layer_3)
+        layer_4 = self.act_postprocess4[3: len(self.act_postprocess4)](layer_4)
+
+        return layer_1, layer_2, layer_3, layer_4
+
+    def forward_flex(self, x):
+        b, c, h, w = x.shape
+
+        pos_embed = self._resize_pos_embed(
+            self.pos_embed.pos_embed, h // self.patch_size[1], w // self.patch_size[0]
+        )
+
+        B = x.shape[0]
+
+        if hasattr(self.patch_embed, "backbone"):
+            x = self.patch_embed.backbone(x)
+            if isinstance(x, (list, tuple)):
+                x = x[-1]  # last feature if backbone outputs list/tuple of features
+
+            x = self.patch_embed.patch_embedding(x)
+        if getattr(self, "dist_token", None) is not None:
+            cls_tokens = self.cls_token.expand(
+                B, -1, -1
+            )  # stole cls_tokens impl from Phil Wang, thanks
+            dist_token = self.dist_token.expand(B, -1, -1)
+            x = torch.cat((cls_tokens, dist_token, x), dim=1)
         else:
-            return inv_depth
+            cls_tokens = self.cls_token.expand(
+                B, -1, -1
+            )  # stole cls_tokens impl from Phil Wang, thanks
+            x = torch.cat((cls_tokens, x), dim=1)
+        x = x + pos_embed
+        x = self.pos_embed.pos_drop(x)
+        x = self.encoder(x)
+        return x
