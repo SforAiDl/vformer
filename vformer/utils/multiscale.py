beed6eab3fa8c6eefd2e220cff2c365966d45d5e
Rishav Mukherji <72412977+Amapocho@users.noreply.github.com>
2022-01-25 14:43:23 +0530

    remove logger

--- vformer/utils/multiscale.py
+++ vformer/utils/multiscale.py
@@ -1,26 +1,9 @@
-import logging
 import torch.nn as nn
-
-def get_logger(name):
-    """
-    Retrieve the logger with the specified name or, if name is None, return a
-    logger which is the root logger of the hierarchy.
-    Args:
-        name (string): name of the logger.
-    """
-    return logging.getLogger(name)
-
-logger = get_logger(__name__)
-
-def round_width(width, multiplier, min_width=1, divisor=1, verbose=False):
+def round_width(width, multiplier, min_width=1, divisor=1):
     if not multiplier:
         return width
     width *= multiplier
     min_width = min_width or divisor
-    if verbose:
-        logger.info(f"min width {min_width}")
-        logger.info(f"width {width} divisor {divisor}")
-        logger.info(f"other {int(width + divisor / 2) // divisor * divisor}")
 
     width_out = max(min_width, int(width + divisor / 2) // divisor * divisor)
     if width_out < 0.9 * width:
