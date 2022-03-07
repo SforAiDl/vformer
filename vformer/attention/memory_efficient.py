710ab0019f4a7bc565ea28657d9d6c76a304576c
alvanli <alvin.ipad2@gmail.com>
2022-03-07 14:01:27 -0500

    Remove unneeded code from attentnio

--- vformer/attention/memory_efficient.py
+++ vformer/attention/memory_efficient.py
@@ -41,11 +41,9 @@ class MemoryEfficientAttention(nn.Module):
         project_out = not (num_heads == 1 and head_dim == dim)
 
         self.num_heads = num_heads
-        self.scale = head_dim ** -0.5
         self.query_chunk_size = query_chunk_size
         self.key_chunk_size = key_chunk_size
 
-        self.attend = nn.Softmax(dim=-1)
         self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
 
         self.to_out = (
