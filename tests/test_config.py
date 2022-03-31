370e6f518cb069fe806d68e015a45aacc6d5d329
Abhijit Deo <72816663+abhi-glitchhg@users.noreply.github.com>
2022-03-31 13:22:38 +0530

    pre-commit

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,5 @@
+import torch
+
 from vformer.config import LazyCall, instantiate
 from vformer.models import PVTSegmentation, SwinTransformer, VanillaViT, ViViTModel2
 
@@ -6,26 +8,41 @@ def test_lazy():
     # classification models
     vanilla_config = LazyCall(VanillaViT)(img_size=224, patch_size=7, n_classes=10)
     swin_config = LazyCall(SwinTransformer)(
-        img_size=28, patch_size=4, in_chans=1, n_classes=3
+        img_size=224,
+        patch_size=4,
+        in_channels=3,
+        n_classes=10,
+        embedding_dim=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        window_size=7,
+        p_dropout=0.2,
     )
     vivit_config = LazyCall(ViViTModel2)(
-        num_frames=16,
-        img_size=(64, 64),
-        patch_t=8,
-        patch_h=4,
-        patch_w=4,
+        img_size=224,
+        in_channels=3,
+        patch_size=16,
+        embedding_dim=192,
+        depth=4,
+        num_heads=3,
+        head_dim=64,
+        num_frames=1,
         num_classes=10,
-        embedding_dim=512,
-        depth=3,
-        num_heads=4,
-        head_dim=32,
-        p_dropout=0.0,
-        in_channels=1,
     )
 
     # dense models
     pvt_config = LazyCall(PVTSegmentation)()
     pvt_config["img_size"] = 224
+    rand_img_tensor = torch.randn(4, 3, 224, 224)
+    rand_vdo_tensor = torch.randn([32, 16, 3, 224, 224])
 
-    for i in locals():
-        instantiate(i)  # should not raise any error
+    vanilla_vit = instantiate(vanilla_config)
+    swin_vit = instantiate(swin_config)
+    vivit = instantiate(vivit_config)
+
+    pvt = instantiate(pvt_config)
+
+    assert vanilla_vit(rand_img_tensor).shape == (4, 10)
+    assert swin_vit(rand_img_tensor).shape == (4, 10)
+    assert pvt(rand_img_tensor).shape == (4, 1, 224, 224)
+    assert vivit(rand_vdo_tensor).shape == (32, 10)
